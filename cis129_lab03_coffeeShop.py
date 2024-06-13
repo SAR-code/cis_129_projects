3d2d371273b8e27b847bff82c9c0e8fc2e1eb7ac
SAR-code <dylanmccallum28@gmail.com>
2024-06-12 19:20:56 -0700

    Coffee shop program functionable

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -8,21 +8,35 @@ of total items purchased and cost to the user.
 
 
 def my_coffee_shop():
-    welcome_msg = """*****Welcome My Coffee and Muffin shop*****"""
-    print( welcome_msg )
+    welcome_msg = '\nMy Coffee and Muffin Shop'
+    receipt_msg = '\nMy Coffee and Muffin Shop Receipt'
     
-    coffee = int(input('How many orders of coffee would you like? '))
-    muffin = int(input('How many orders of muffins would you like? '))
+    border = '**********************************'
     
-    coffee_price = 5*float(coffee)
-    muffin_price = 4*float(muffin)
     
-    item_total = coffee_price + muffin_price
+    print( border, welcome_msg )
     
-    print('Number of coffees bought:', coffee, 'Number of muffins bought:', muffin)
+    coffee_count = int(input('Number of coffees bought? \n'))
+    muffin_count = int(input('Number of muffins bought? \n'))
     
-    print('Coffee total:', f"${coffee_price}", 'Muffin total:'
-                , f"${muffin_price}", 'Item total:', f"${item_total}")
+    print(border)
+    
+    coffee_price = 5*float(coffee_count)
+    muffin_price = 4*float(muffin_count)
+    
+    sales_tax = (coffee_price + muffin_price) * .06
+    item_total = (coffee_price + muffin_price) + sales_tax    
+    
+    print( border, receipt_msg )
+    
+    
+    print(f"{coffee_count} Coffee at $5 each:", f"${coffee_price}", 
+                f"\n{muffin_count} Muffins at $4 each:", f"${muffin_price}",
+                f"\n6% tax: ${sales_tax} \n-------", '\nTotal:',
+                f"${item_total}")
+    
+    
+    print( border )
     
 
 
