e3614a78208b736f68611be943aed5388521ac5b
SAR-code <dylanmccallum28@gmail.com>
2024-06-23 00:23:48 -0700

    hotdog test success

--- cis129_lab06_hotdog_cookout.py
+++ cis129_lab06_hotdog_cookout.py
@@ -13,6 +13,59 @@ cookout and the amount of hotdogs each person is given.
 
 """
 
-# declaring local variable for total hotdogs
+# declaring global variable for total hotdogs
 
 total_hotdogs = 0
+
+# declaring function to get the total number of hotdogs
+
+def get_total_hotdogs():
+    
+    # declaring local variables for calculations
+    
+    attendees = 0   # number of people attending
+    hotdogs = 0     # number of hotdogs per person
+    
+    # receiving user input on number of attendees and hotdogs
+    
+    attendees = int(input("Maximum amount of people attending?: "))
+    hotdogs = int(input("How many hotdogs per attendee?: "))
+    
+    # calculates the total between hotdogs and attendees
+    
+    total = attendees * hotdogs
+    
+    return total    # returns the total
+    
+total_hotdogs = get_total_hotdogs()
+
+# declaring function to get results of remaining hotdogs and buns leftover
+
+def show_results(total):
+    
+    # initializing required constants to simulate packaging
+    
+    DOGS = 10
+    BUNS = 8
+    
+    # declaring local variables for leftover items
+    
+    dogs_left = 0
+    buns_left = 0
+    
+    # declaring local variables for minimum required items
+    
+    min_dogs = 0
+    min_buns = 0
+    
+    # calculating the minimum items required and the leftover ingredients
+    
+    dogs_left = (DOGS - total % DOGS) % DOGS
+    min_dogs = (total / DOGS) + (0**(0** dogs_left))
+    
+    # displays message to the user
+    
+    print(f"Minimum packages of hotdogs needed {min_dogs}",
+           f"There will be {dogs_left} leftover")
+
+show_results(total_hotdogs)
