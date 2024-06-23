94ea939692513a05e8d5d88b666598c3d14aabe9
SAR-code <dylanmccallum28@gmail.com>
2024-06-22 23:44:18 -0700

    initialized file

---
+++ cis129_lab06_hotdog_cookout.py
@@ -0,0 +1,18 @@
+# cis129_lab06_hotdog_cookout.py
+"""
+Module 6 Lab-6
+Dylan McCallum
+06/22/2024
+
+Hotdog Cookout Program
+
+This program calculates the number of hotdogs and the number of buns required
+for a cookout with the minimum amount of leftover ingredients. Program will
+request basic information from the user on the number of guest attending the
+cookout and the amount of hotdogs each person is given.
+
+"""
+
+# declaring local variable for total hotdogs
+
+total_hotdogs = 0
