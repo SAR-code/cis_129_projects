4099756ffd84a8769128d86eeb4b3087a28c676b
SAR-code <dylanmccallum28@gmail.com>
2024-06-30 16:31:24 -0700

    Code functions

--- cis129_dylanMccallum_lab07.py
+++ cis129_dylanMccallum_lab07.py
@@ -41,11 +41,11 @@ def main():
         
         # call to roll_dice
         
-        winners_name = roll_dice(p1_number, p2_number, player_one, player_two)
+        winner_name = roll_dice(p1_number, p2_number, player_one, player_two, winners_name)
         
         # call to display_info
         
-        display_info(winners_name)
+        display_info(winner_name)
         
         end_program = input('Do you want to end program? (yes/no): ')
     
@@ -53,17 +53,27 @@ def main():
 
 def input_names(player_1, player_2):
     
+    player_1 = input("Player One enter your name: ")
+    player_2 = input("Player Two enter your name: ")
     
     return player_1, player_2
     
 # this function gets the random values
-def roll_dice(p1_num, p2_num, player_one, player_two):
+def roll_dice(p1_num, p2_num, player_one, player_two, winner_name):
     
     p1_num = random.randint(1, 6)
     p2_num = random.randint(1, 6)
     
-    #if (p1_num == p2_num):
-        
+    if (p1_num == p2_num):
+        winner_name = 'TIE'
+    else:
+        if (p1_num > p2_num):
+            winner_name = player_one
+        else:
+            winner_name = player_two
+    
+    return winner_name
+
 # this function displays the winner
 
 def display_info(winners_name):
