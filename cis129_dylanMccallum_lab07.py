0a1fa214f3987b42d5ccdbd45d7b8da94b860476
SAR-code <dylanmccallum28@gmail.com>
2024-06-30 15:24:27 -0700

    code shell completed

--- cis129_dylanMccallum_lab07.py
+++ cis129_dylanMccallum_lab07.py
@@ -7,7 +7,34 @@ Dylan McCallum
 This program contains lab 7-3 which simulates a dice game.
 
 """
+import random
 
+def main():
+    print()
+    
+    # initialize variables
+    
+    # call to inputName
+    
+    # while loop to run program again
+    
+    while end_program == 'no':
+        
+        # populate variables
+        
+        # call to roll_dice
+        
+        # call to display_info
+        
+        end_program = input('Do you want to end program? (yes/no): ')
+    
+# this function get the players names
 
+# this function gets the random values
+
+# this function displays the winner
+
+# calls main
+main()
 
 
