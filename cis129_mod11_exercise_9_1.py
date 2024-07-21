1d00fceadc88758935f8c521e9b837245a842fec
SAR-code <dylanmccallum28@gmail.com>
2024-07-21 12:56:43 -0700

    Grade.txt created

--- cis129_mod11_exercise_9_1.py
+++ cis129_mod11_exercise_9_1.py
@@ -10,3 +10,18 @@ This program calculates grades from the class and returns the class average
 and then stores the grades in a text file.
 
 """
+# declare main function to hold program operations
+
+def main():
+    print("Hello")
+    
+    # create a grades.txt to write and store grades into
+    with open('grades.txt', mode='w') as grades:
+        grades.write('100 Dylan 98\n')
+        grades.write('200 Alex 79\n')
+        grades.write('300 Dan 84\n')
+        
+
+# invokes main function
+
+main()
