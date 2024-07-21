3eb879ae916f18a619a278cc2d7bc802b798b55c
SAR-code <dylanmccallum28@gmail.com>
2024-07-21 12:25:00 -0700

    9.1 file initialized

---
+++ cis129_mod11_exercise_9_1.py
@@ -0,0 +1,12 @@
+# cis129_mod11_exercise_9_1.py
+"""
+Module 11 exercise 9.1
+Dylan McCallum
+07/21/2024
+
+Writing Grades To A Plain Text File
+
+This program calculates grades from the class and returns the class average
+and then stores the grades in a text file.
+
+"""
