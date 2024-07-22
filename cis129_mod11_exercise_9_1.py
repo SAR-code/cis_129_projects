79b501b6496cd88f9fa24e79db2a1ff8a7cb42ea
SAR-code <dylanmccallum28@gmail.com>
2024-07-21 18:20:52 -0700

    Calculation works

--- cis129_mod11_exercise_9_1.py
+++ cis129_mod11_exercise_9_1.py
@@ -10,6 +10,7 @@ This program calculates grades from the class and returns the class average
 and then stores the grades in a text file.
 
 """
+
 # declare global variables and objects
 
 # declare class object to hold the student name and grade
@@ -18,11 +19,11 @@ class Student:
     def __init__(self, name=str, grade=int):
         self.name = name
         self.grade = grade
-        
+    
+    # returns string representation of the Student Object
     def __repr__(self):
-        return (f"Name: {self.name}\n",
-                f"Grade: {self.grade}\n"
-                )
+        return f"{self.name}: {self.grade}"
+                
 
 # declare main function to hold program operations
 
@@ -33,36 +34,47 @@ def main():
     
     stop_grading = 'no'
     class_grades = []
+    grade_list = []
     count = 1
     
     
     while stop_grading == 'no':
         
+        # while loop to append students and grades to a list
+        
         while count != 0:
             
-            # attemp to receive correct value inputs for grades
+            # attempt to receive correct value inputs for grades
             try:
                 student_name = input("Enter the student's name: ")
                 grade = int(input(f"What is {student_name}'s grade?: "))
-                stop_grading = input("Stop grading? (yes/no): ")
+                stop_grading = input("Stop grading? ( yes/no ): ")
             
+                # adds student to the class list
                 if stop_grading == 'no':
                     student = Student(student_name, grade)
+                    grade_list.append(grade)
                     class_grades.append(student)
                     count += 1
                 else:
+                    
+                    # will add the last student entered into the list
+                    student = Student(student_name, grade)
+                    grade_list.append(grade)
+                    class_grades.append(student)
                     count = 0
             
-            
-            
+            # catches and raises incorrect input
             except ValueError:
                 print("You must enter a name for student or number for grade")
         
             else:
-                for obj in class_grades:
-                    print(obj.name, obj.grade, sep=' ')
+                print(class_grades)
+                get_class_total(grade_list)
+
     
-      
+    
+    
     # create a grades.txt to write and store grades into
     
     # with open('grades.txt', mode='w') as grades:
@@ -70,7 +82,11 @@ def main():
     #     grades.write('200 Alex 79\n')
     #     grades.write('300 Dan 84\n')
 
-
+def get_class_total(grades=list):
+    class_sum = sum(grades)
+    class_avg = class_sum / len(grades)
+    print(f"The class average is {class_avg:.2f}")
+    return class_avg
 
 # invokes main function
 
