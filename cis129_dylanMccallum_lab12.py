9d786aca98446e2dc4a231b32351090d4286b2f4
SAR-code <dylanmccallum28@gmail.com>
2024-07-28 18:27:43 -0700

    Class Pet with methods created

--- cis129_dylanMccallum_lab12.py
+++ cis129_dylanMccallum_lab12.py
@@ -15,13 +15,49 @@ class Pet:
     """Class Pet with read-write properties."""
     
     def __init__(self, name=str, type=str, age=int):
-        """Initialize each attribute"""
+        """Initialize each attribute."""
         
         self.name = name        # pet name
         self.type = type        # type of pet
         self.age = age          # pet age
     
     def __repr__(self):
+        """Returns string representation."""
+        
         return f"A {self.type} named {self.name} whose {self.age} years old"
     
+    @property
+    def name(self):
+        """Returns the name of the pet."""
+        
+        return self._name
     
+    @name.setter
+    def name(self, name):
+        """Sets the pet name."""
+        
+        self._name = name    
+    
+    @property
+    def type(self):
+        """Returns the pet type."""
+        
+        return self._type
+    
+    @type.setter
+    def type(self, type):
+        """Sets the pet type."""
+        
+        self._type = type
+    
+    @property
+    def age(self):
+        """Returns the pet age."""
+        
+        return self._age 
+    
+    @age.setter
+    def age(self, age):
+        """Sets the pet age."""
+        
+        self._age = age
